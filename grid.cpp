666dc04d042c236853679bee5b773e8c920eda75
MaxEaton <maximiliano.k.eaton@gmail.com>
2022-09-28 18:15:37 -0600

    special case solved

--- grid.cpp
+++ grid.cpp
@@ -4,7 +4,7 @@
 #include <bitset>
 #include <x86intrin.h>
 
-#define L 5
+#define L 4
 #define BINS 64
 #define BINMASK 0x10307
 
@@ -43,8 +43,33 @@ struct board {
                 return;
             }
         }
-        prevBoards.push_back(cells);
+        if (comb(n)) {
+            prevBoards.push_back(cells);
+        }
+        
         return;
+    }
+
+    bool comb(int n) {
+        flip();
+        for (std::vector<uint64_t> prevBoards : boards[n-2]) {
+            for (uint64_t prevBoard : prevBoards) {
+                if (prevBoard == cells) {
+                    rotate();
+                    rotate();
+                    translate();
+                    if (prevBoard == cells) {
+                        flip();
+                        return false;
+                    }
+                    rotate();
+                    rotate();
+                    translate();
+                }
+            }
+        }
+        flip();
+        return true;
     }
 
     void print() {
@@ -81,6 +106,20 @@ struct board {
         return;
     }
 
+    void flip() {
+        uint64_t result = 0;
+        uint64_t masks[8] = {0xff00000000000000, 0x00ff000000000000, 0x0000ff0000000000, 0x000000ff00000000, 
+                             0x00000000ff000000, 0x0000000000ff0000, 0x000000000000ff00, 0x00000000000000ff};
+        for(int i=0; i<8; i++) {
+            uint64_t selected = _pext_u64(cells, masks[i]);
+            result |= selected << (i * 8);
+        }
+        cells = result;
+        transform();
+        return;
+
+    }
+
     void transform() {
         translate();
 
@@ -99,15 +138,15 @@ struct board {
 
 std::vector<uint64_t> board::boards[L*L-2][BINS] = {};
 
-uint64_t nextComboPossible(uint64_t cell) {
+uint64_t nextComboPossible(uint64_t combo) {
     // find index of first 0 (from msb) in cells
-    int leading_ones = __builtin_clzll(~cell);
+    int leading_ones = __builtin_clzll(~combo);
     // if top of number is ones, reset
     if(leading_ones != 0) {
         // get mask of leading ones
         uint64_t leading_ones_mask = (int64_t)0x8000000000000000 >> (leading_ones - 1);
         // remove leading ones
-        uint64_t cell_advance = cell & ~leading_ones_mask;
+        uint64_t cell_advance = combo & ~leading_ones_mask;
 
         if(cell_advance == 0) {
             return 0;
@@ -117,14 +156,13 @@ uint64_t nextComboPossible(uint64_t cell) {
         // create mask to advance one
         uint64_t advance_mask = (int64_t)0x8000000000000000 >> (leading_ones + 1);
         
-        cell = cell_advance ^ (advance_mask >> (advance_leading_zeros - leading_ones - 1));
+        combo = cell_advance ^ (advance_mask >> (advance_leading_zeros - leading_ones - 1));
     } else {
         // advance first 1
-        int first_1_index = 63 - __builtin_clzll(cell);
-        cell ^= ((uint64_t)3 << first_1_index);
+        int first_1_index = 63 - __builtin_clzll(combo);
+        combo ^= ((uint64_t)3 << first_1_index);
     }
-
-    return cell;
+    return combo;
 }
 
 int main() {
@@ -144,22 +182,21 @@ int main() {
             combo = nextComboPossible(combo);
         }
 
-        std::cout <<  "======================" << std::endl << "n = " << i << std::endl << std::endl;
+        std::cout <<  "======================" << std::endl;
         int count = 0;
         for (int j=0; j<BINS; j++) {
             auto& boards = board::boards[i - 2][j];
-            for(uint64_t cells : boards) {
+            for (uint64_t cells : boards) {
                 //board::fromCells(cells).print();
                 //std::cout << std::endl;
                 count++;
             }
         }
         std::cout << "n = " << i << ", count = " << count << std::endl << std::endl;
-
         counts[i] = count;
     }
 
-    std::cout <<  "====================" << std::endl << "L = " << L << std::endl;
+    std::cout <<  "======================" << std::endl << "L = " << L << std::endl;
     int sum = 0;
     for(int i = 0; i <= L*L; i++) {
         std::cout << "n = " << i << ",\tcount = " << counts[i] << std::endl;
